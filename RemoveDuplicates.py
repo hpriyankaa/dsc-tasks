3c599e47b60dd797ac4c557262dbc568c06517c0
Priyanka <148324851+hpriyankaa@users.noreply.github.com>
2024-10-07 12:45:28 +0530

    Create RemoveDuplicates.py

---
+++ RemoveDuplicates.py
@@ -0,0 +1,10 @@
+def removeDuplicate(nums):
+    i = 0
+    while i < len(nums) - 1:
+        if nums[i] == nums[i + 1]:
+            nums.remove(nums[i])
+        else:
+            i += 1 
+    return nums
+num=removeDupli([2,3,4,4,6,7,7])
+print(num)
